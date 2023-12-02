c9dfaeb05324dffc3310122fbd84a1c73d141552
vchandela <vivekchandela@sharechat.co>
2023-12-03 00:45:13 +0530

    chore: refactor q2

--- q2_part2/src/main.rs
+++ q2_part2/src/main.rs
@@ -4,36 +4,28 @@ use std::cmp;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let contents = fs::read_to_string(file_path)
+    let file_path: &String = &args[1];
+    let contents: String = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
-    let mut sum = 0;
+    let mut sum: i32 = 0;
     for line in contents.lines() {
         // println!("{}", line);
         let parts: Vec<&str> = line.split(' ').collect();
         // println!("{:?}", parts);
-        let part_len = parts.len();
-        let mut maxx_red = 0;
-        let mut maxx_green = 0;
-        let mut maxx_blue = 0;
+        let part_len: usize = parts.len();
+        let (mut maxx_red, mut maxx_green, mut maxx_blue): (i32, i32, i32) = (0, 0, 0);
         for idx in 1..part_len {
-            let sub_string = parts.get(idx).unwrap();
-            // In case of ParseIntError, set num to 0
-            let num = parts.get(idx-1).unwrap().parse::<i32>().unwrap_or(0);
-            match *sub_string {
-                "red" => maxx_red = cmp::max(maxx_red,num),
-                "red," => maxx_red = cmp::max(maxx_red,num),
-                "red;" => maxx_red = cmp::max(maxx_red,num),
-                "green" => maxx_green = cmp::max(maxx_green,num),
-                "green," => maxx_green = cmp::max(maxx_green,num),
-                "green;" => maxx_green = cmp::max(maxx_green,num),
-                "blue" => maxx_blue = cmp::max(maxx_blue,num),
-                "blue," => maxx_blue = cmp::max(maxx_blue,num),
-                "blue;" => maxx_blue = cmp::max(maxx_blue,num),
+            let sub_str: &str = parts.get(idx).unwrap_or(&"");
+            let num_str: &str = parts.get(idx-1).unwrap_or(&"");
+            let num: i32 = num_str.parse::<i32>().unwrap_or(0);
+            match sub_str {
+                "red" | "red," | "red;" => maxx_red = cmp::max(maxx_red,num),
+                "green" | "green," | "green;" => maxx_green = cmp::max(maxx_green,num),
+                "blue" | "blue," | "blue;" => maxx_blue = cmp::max(maxx_blue,num),
                 _ => {}, // Do nothing for other cases
             }
         }
-        let power = maxx_red * maxx_green * maxx_blue;
+        let power: i32 = maxx_red * maxx_green * maxx_blue;
         sum += power;
     }
     println!("{}", sum);
